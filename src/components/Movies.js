3b56943a0f74b78c0be73942ee564f2949f819fc
Derek Araujo <derekaraujo19@gmail.com>
2021-12-20 00:31:07 -0500

    Assignment Complete

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  // console.log(movies);
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie) => (
+          <div key={movie.title}>
+            <h3>Name: {movie.title}</h3>
+            <div>Time: {movie.time}</div>
+            <div>Genres:</div>
+            <ul>
+              {movie.genres.map((genre, index) => (
+                <li key={index}>
+                  {genre}
+                </li>
+              ))}
+            </ul>
+          </div>
+      ))}
+    </div>
+  );
 }
 
 export default Movies;
